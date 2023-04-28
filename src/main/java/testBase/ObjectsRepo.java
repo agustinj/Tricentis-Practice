fbc9a5f59ecf519ad24c60d3965ce97779672fbf
agustinj <agustin.jauregui2@gmail.com>
2023-04-28 11:32:50 +0200

    Moved testng.xml Instantiated Objects in ObjectsRepo and created main methods in TestBase with annotations

--- src/main/java/testBase/ObjectsRepo.java
+++ src/main/java/testBase/ObjectsRepo.java
@@ -1,4 +1,27 @@
 package testBase;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import org.openqa.selenium.WebDriver;
+import pageObjects.EnterProductDataPageObjects;
+import pageObjects.HomePageObjects;
+import pageObjects.SelectPriceOptionsPageObjects;
+import reusableComponents.CommonMethods;
+
 public class ObjectsRepo {
+    public class ObjectsRepo {
+        //Initialize main objects
+        public static WebDriver driver;
+        public static ExtentReports extent;
+        public static ExtentTest test;
+        public static HomePageObjects homepage;
+        //Initialize objects specific for the project
+        public static EnterVehicalDataPageObjects vehData;
+        public static EnterInsurantDataPageObjects insData;
+        public static EnterProductDataPageObjects prodData;
+        public static SelectPriceOptionsPageObjects priceOptions;
+
+        
+        public CommonMethods cm = new CommonMethods();
+    }
 }
